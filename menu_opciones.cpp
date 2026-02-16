7e75e4db70fc6717c00456422ddd535cf7780d99
Rodrix-ra34 <00167625@uca.edu.com>
2026-02-15 19:36:57 -0500

    add: se agrego el menu para escoger entre modo manual o aleatoreo

---
+++ menu_opciones.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+void menuJugar()
+{
+    cout << "|==============================" << endl;
+    cout << "|       MODO DE JUEGO          |" << endl;
+    cout << "|==============================|" << endl;
+    cout << "| 1. Seleccion manual.         |" << endl;
+    cout << "| 2. Seleccion aleatoria.      |" << endl;
+    cout << "|______________________________|" << endl;
+    cout << " Ingrese una opcion:       " << endl;
+}
