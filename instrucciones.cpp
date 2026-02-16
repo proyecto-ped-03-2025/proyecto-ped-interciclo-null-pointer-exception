0a87ef194286c78dc6e76ab48fac0bf841ea2315
Rodrix-ra34 <00167625@uca.edu.com>
2026-02-15 19:45:30 -0500

    add: se agregaron las instrucciones del juego

---
+++ instrucciones.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+void instrucciones()
+{
+    cout << " ____________________________________________________________________________________" << endl;
+    cout << "|                                                                                    |" << endl;
+    cout << "|                                    INSTRUCCIONES                                   |" << endl;
+    cout << "|____________________________________________________________________________________|" << endl;
+    cout << "|                                                                                    |" << endl;
+    cout << "|  Para poder jugar piedra papel o tijeras se hace lo siguiente:                     |" << endl;
+    cout << "|  1.Cada partida se hace entre 2 personas.                                          |" << endl;
+    cout << "|  2. los jugadores podran escoger una entre 3 opciones: piedra, papel o tijeras.    |" << endl;
+    cout << "|  3. La Piedra solo puede ganarle a las tijeras.                                    |" << endl;
+    cout << "|     El papel solo puede ganarle a la piedra.                                       |" << endl;
+    cout << "|     Las tijeras solo pueden ganarle al papel.                                      |" << endl;
+    cout << "|____________________________________________________________________________________|" << endl;
+}
