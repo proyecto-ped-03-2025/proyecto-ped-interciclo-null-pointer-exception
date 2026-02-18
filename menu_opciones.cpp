9e2db141994193ac1b26584e143cc22a1543c158
Rodrix-ra34 <00167625@uca.edu.com>
2026-02-17 22:31:28 -0600

    add: se le agrego codigo ansii para colores en los menus

--- menu_opciones.cpp
+++ menu_opciones.cpp
@@ -6,15 +6,20 @@ using namespace std;
 
 void menuJugar()
 {
-    cout << "|==============================" << endl;
-    cout << "|       MODO DE JUEGO          |" << endl;
-    cout << "|==============================|" << endl;
-    cout << "| 1. Seleccion manual.         |" << endl;
-    cout << "| 2. Seleccion aleatoria.      |" << endl;
-    cout << "|______________________________|" << endl;
-    cout << " Ingrese una opcion:       " << endl;
-}
+    cout << "\033[34m|==============================|\033[0m" << endl;
 
+    cout << "\033[34m|\033[35m       MODO DE JUEGO          \033[34m|\033[0m" << endl;
+
+    cout << "\033[34m|==============================|\033[0m" << endl;
+
+    cout << "\033[34m| \033[32m1. Seleccion manual.         \033[34m|\033[0m" << endl;
+
+    cout << "\033[34m| \033[32m2. Seleccion aleatoria.      \033[34m|\033[0m" << endl;
+
+    cout << "\033[34m|______________________________|\033[0m" << endl;
+
+    cout << "\033[33m Ingrese una opcion:       \033[0m" << endl;
+}
 void menuOpciones()
 {
     cout << "1. Piedra" << endl;
