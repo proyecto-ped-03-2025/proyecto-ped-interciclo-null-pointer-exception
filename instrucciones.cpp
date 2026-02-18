201b99c79490acc79406a9c2befaa9eb9875b3a6
Rodrix-ra34 <00167625@uca.edu.com>
2026-02-17 19:04:51 -0600

    se uso codigo ANSII para agregarle color a las instrucciones

--- instrucciones.cpp
+++ instrucciones.cpp
@@ -6,16 +6,27 @@ using namespace std;
 
 void instrucciones()
 {
-    cout << " ____________________________________________________________________________________" << endl;
-    cout << "|                                                                                    |" << endl;
-    cout << "|                                    INSTRUCCIONES                                   |" << endl;
-    cout << "|____________________________________________________________________________________|" << endl;
-    cout << "|                                                                                    |" << endl;
-    cout << "|  Para poder jugar piedra papel o tijeras se hace lo siguiente:                     |" << endl;
-    cout << "|  1.Cada partida se hace entre 2 personas.                                          |" << endl;
-    cout << "|  2. los jugadores podran escoger una entre 3 opciones: piedra, papel o tijeras.    |" << endl;
-    cout << "|  3. La Piedra solo puede ganarle a las tijeras.                                    |" << endl;
-    cout << "|     El papel solo puede ganarle a la piedra.                                       |" << endl;
-    cout << "|     Las tijeras solo pueden ganarle al papel.                                      |" << endl;
-    cout << "|____________________________________________________________________________________|" << endl;
+    cout << "\033[34m ____________________________________________________________________________________\033[0m" << endl;
+
+    cout << "\033[34m|\033[0m                                                                                    \033[34m|\033[0m" << endl;
+
+    cout << "\033[34m|\033[35m                                    INSTRUCCIONES                                   \033[34m|\033[0m" << endl;
+
+    cout << "\033[34m|____________________________________________________________________________________|\033[0m" << endl;
+
+    cout << "\033[34m|\033[0m                                                                                    \033[34m|\033[0m" << endl;
+
+    cout << "\033[34m|  \033[33mPara poder jugar piedra papel o tijeras se hace lo siguiente:\033[34m                     |\033[0m" << endl;
+
+    cout << "\033[34m|  \033[32m1. Cada partida se hace entre 2 personas.\033[34m                                         |\033[0m" << endl;
+
+    cout << "\033[34m|  \033[32m2. Los jugadores podran escoger una entre 3 opciones: piedra, papel o tijeras.\033[34m    |\033[0m" << endl;
+
+    cout << "\033[34m|  \033[32m3. La Piedra solo puede ganarle a las tijeras.\033[34m                                    |\033[0m" << endl;
+
+    cout << "\033[34m|     \033[32mEl papel solo puede ganarle a la piedra.\033[34m                                       |\033[0m" << endl;
+
+    cout << "\033[34m|     \033[32mLas tijeras solo pueden ganarle al papel.\033[34m                                      |\033[0m" << endl;
+
+    cout << "\033[34m|____________________________________________________________________________________|\033[0m" << endl;
 }
