8ebbd8a3528420c6318af664a395f5b1ed8c9c6e
Rodrix-ra34 <00167625@uca.edu.com>
2026-02-18 11:25:19 -0600

    add: se han corregido unas partes del codigo para que muestre la jugada de los jugadores en la parte aleatorea como a su vez se agrego la llamada de la funcion esperar

--- jugar.cpp
+++ jugar.cpp
@@ -46,6 +46,7 @@ void jugar(Jugador *frente, Jugador *cola)
 {
     if (estaVacio(frente))
     {
+        esperar();
         return;
     }
 
@@ -54,10 +55,11 @@ void jugar(Jugador *frente, Jugador *cola)
 
     jugador1 = frente;
 
-    if (frente == cola)
+    if (frente == cola) // Verifica si hay un solo jugador en la lista circular, si es asi no se puede jugar
     {
         cout << "Se necesita mas de un jugador para poder empezar" << endl
              << endl;
+        esperar();
         return;
     }
 
@@ -76,12 +78,12 @@ void jugar(Jugador *frente, Jugador *cola)
     }
     else
     {
-        cout << "Opcion invalida." << endl;
+        cout << "\033[31mOpcion invalida.\033[0m" << endl;
     }
 
     do
     {
-        jugador2 = jugador1->siguiente;
+        jugador2 = jugador1->siguiente; // El jugador 2 sera el que sigue del jugador 1 para que se enfrenten entre si
         cout << "Partida entre " << jugador1->jugador << " y " << jugador2->jugador << endl
              << endl;
 
@@ -101,38 +103,54 @@ void jugar(Jugador *frente, Jugador *cola)
 
                 if (opcion == opcion2)
                 {
-                    cout << "Esto es un empate" << endl;
+                    cout << "Esto es un empate" << endl; // si ambos jugadores escogen la misma opcion, se declara un empate
+                    esperar();
                 }
                 else if ((opcion == 1 && opcion2 == 3) || (opcion == 2 && opcion2 == 1) || (opcion == 3 && opcion2 == 2))
+                // se verifican todos los casos en los que pueda ganar el jugador 1
                 {
+
                     cout << "Ha ganado : " << jugador1->jugador << endl
                          << endl;
-                    jugador1->puntaje++;
+                    jugador1->puntaje++; // Si el jugador 1 gana, se le suma un punto a su puntaje
+                    esperar();
                 }
                 else
                 {
                     cout << "Ha ganado : " << jugador2->jugador << endl
                          << endl;
-                    jugador2->puntaje++;
+                    jugador2->puntaje++; // Caso contrario el jugador 2 gana puntos
+                    esperar();
                 }
             }
             else if (modo == 2)
             {
+
                 int num2 = (rand() % 3) + 1;
-                int num = (rand() % 3) + 1;
+                int num = (rand() % 3) + 1;                             // Con esto quiere decir que va a generar un numero aleatoreo entre 1 al 3
+                const char *jugadas[] = {"Piedra", "Papel", "Tijeras"}; // Se crea un arreglo de strings para mostrar la jugada que hizo cada jugador
+
+                cout << jugador1->jugador << " jugo: " << jugadas[num - 1] << endl;/* Se muestra la jugada del jugador 1,
+                 se le resta 1 al numero generado para que coincida con el indice del arreglo*/
+                cout << jugador2->jugador << " jugo: " << jugadas[num2 - 1] << endl;
+
                 if (num == num2)
                 {
                     cout << "Esto es un empate" << endl;
+                    esperar();
                 }
                 else if ((num == 1 && num2 == 3) || (num == 2 && num2 == 1) || (num == 3 && num2 == 2))
                 {
+
                     cout << "Ha ganado : " << jugador1->jugador << endl;
                     jugador1->puntaje++;
+                    esperar();
                 }
                 else
                 {
                     cout << "Ha ganado : " << jugador2->jugador << endl;
                     jugador2->puntaje++;
+                    esperar();
                 }
             }
             else
@@ -140,11 +158,12 @@ void jugar(Jugador *frente, Jugador *cola)
                 cout << "opcion invalida" << endl;
             }
 
-            jugador2 = jugador2->siguiente;
+            jugador2 = jugador2->siguiente; /* El jugador 2 se mueve al siguiente jugador para que se enfrente con el jugador 1,
+            hasta que el jugador 2 vuelva a ser el mismo que el jugador 1, lo que quiere decir que ya se enfrentaron con todos los jugadores*/
 
-        } while (jugador2 != frente);
+        } while (jugador2 != frente); // se va a mover el jugador 2 hasta que este vuelva a ser el mismo que el jugador 1
 
-        jugador1 = jugador1->siguiente;
+        jugador1 = jugador1->siguiente; // una vez pase lo anterior, el jugador 1 pasa a ser el siguiente jugador
 
-    } while (jugador1 != cola);
+    } while (jugador1 != cola); // se va a mover el jugador 1 hasta que llegue a la cola, lo que quiere decir que ya se enfrentaron todos los jugadores entre si
 }
